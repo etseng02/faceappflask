62112371740d05c26d7c6f312126dbb592111e7a
Eddie <eddietseng@outlook.com>
2019-12-18 14:07:39 -0500

    Replaced test route with an algorithm to detect faces in a photo and return photo with boxes around the faces

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from PIL import Image
+from PIL import Image, ImageDraw
 import face_recognition
 from flask_cors import CORS
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
@@ -9,20 +10,66 @@ CORS(app)
 
 @app.route('/test')
 def index():
-  image = face_recognition.load_image_file("./test/biden.jpg")
-  face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+  obama_image = face_recognition.load_image_file("./test/obama.jpg")
+  obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
-  print("I found {} face(s) in this photograph.".format(len(face_locations)))
+  # Load a second sample picture and learn how to recognize it.
+  biden_image = face_recognition.load_image_file("./test/biden.jpg")
+  biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
-  for face_location in face_locations:
+  known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+  ]
+  known_face_names = [
+      "Barack Obama",
+      "Joe Biden"
+  ]
 
-    # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+  unknown_image = face_recognition.load_image_file("./test/alex-lacamoire.png")
 
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    pil_image.show()
+  # Find all the faces and face encodings in the unknown image
+  face_locations = face_recognition.face_locations(unknown_image)
+  face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+  pil_image = Image.fromarray(unknown_image)
+  # Create a Pillow ImageDraw Draw instance to draw with
+  draw = ImageDraw.Draw(pil_image)
+
+  # Loop through each face found in the unknown image
+  for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+    name = "Unknown"
+
+    # If a match was found in known_face_encodings, just use the first one.
+    # if True in matches:
+    #     first_match_index = matches.index(True)
+    #     name = known_face_names[first_match_index]
+
+    # Or instead, use the known face with the smallest distance to the new face
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+    best_match_index = np.argmin(face_distances)
+    if matches[best_match_index]:
+        name = known_face_names[best_match_index]
+
+    # Draw a box around the face using the Pillow module
+    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+    # Draw a label with a name below the face
+    text_width, text_height = draw.textsize(name)
+    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+    draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+    # Remove the drawing library from memory as per the Pillow docs
+  del draw
+
+  # Display the resulting image
+  pil_image.show()
+
+    # You can also save a copy of the new image to disk if you want by uncommenting this line
+    # pil_image.save("image_with_boxes.jpg")
   return "Hello, World!"
 
 @app.route('/api/recognize', methods=['GET'])
