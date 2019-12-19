d4030ce5d27244f4ce08b049662923f478fb83cd
Eddie <eddietseng@outlook.com>
2019-12-19 06:14:46 -0500

    Tested image return of results. Sucessful

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Faceapp Flask Face Recognition Back End
 
-source env/bin/activate
+1. Install all requirements in requirements.txt
+2. source env/bin/activate
+3. 'python3 app.py' to run server
+
+## Requirements
+  Click==7.0
+  dlib==19.19.0
+  face-recognition==1.2.3
+  face-recognition-models==0.3.0
+  Flask==1.1.1
+  Flask-Cors==3.0.8
+  itsdangerous==1.1.0
+  Jinja2==2.10.3
+  MarkupSafe==1.1.1
+  numpy==1.17.4
+  Pillow==6.2.1
+  six==1.13.0
+  virtualenv==16.7.9
+  Werkzeug==0.16.0
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, send_from_directory
 from PIL import Image, ImageDraw
 import face_recognition
 from flask_cors import CORS
@@ -76,6 +76,7 @@ def index():
   
   
 app.config["IMAGE_UPLOADS"] = "/Users/eddietseng/Developer/faceappflask/uploads"
+app.config["IMAGE_RESULTS"] = "/Users/eddietseng/Developer/faceappflask/results"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG"]
 
 def allowed_image(filename):
@@ -91,6 +92,8 @@ def allowed_image(filename):
 
 @app.route('/api/recognize', methods=['POST'])
 def recognize():
+
+  print(request)
 
   if request.method == "POST":
 
@@ -110,7 +113,66 @@ def recognize():
           image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
           print ("image saved")
 
-  print(image)
+          obama_image = face_recognition.load_image_file("./test/obama.jpg")
+          obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+          # Load a second sample picture and learn how to recognize it.
+          biden_image = face_recognition.load_image_file("./test/biden.jpg")
+          biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+          known_face_encodings = [
+            obama_face_encoding,
+            biden_face_encoding
+          ]
+          known_face_names = [
+            "Barack Obama",
+            "Joe Biden"
+          ]
+
+          unknown_image = face_recognition.load_image_file('./uploads/{}'.format(filename))
+
+          face_locations = face_recognition.face_locations(unknown_image)
+          face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+          pil_image = Image.fromarray(unknown_image)
+          draw = ImageDraw.Draw(pil_image)
+
+          # Loop through each face found in the unknown image
+          for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+            name = "Unknown"
+
+            # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+              name = known_face_names[best_match_index]
+
+            # Draw a box around the face using the Pillow module
+            draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+            # Draw a label with a name below the face
+            text_width, text_height = draw.textsize(name)
+            draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+            draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+          # Remove the drawing library from memory as per the Pillow docs
+          del draw
+
+          # Display the resulting image
+          pil_image.save(os.path.join(app.config["IMAGE_RESULTS"], filename))
+
+
+          return send_file('/Users/eddietseng/Developer/faceappflask/results/{}'.format(filename), attachment_filename=filename, cache_timeout=0)
+          # print(filename)
+          # return send_from_directory('uploads', filename)
 
   return "Hello world"
 
